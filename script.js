595e335b39224cbe5ec73f519effc2ad68a7c682
Vsevolod <sevapan@gmail.com>
2019-12-16 21:38:09 +0500

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log("2" === 2);
+
+let isChecked = true,
+    isFalse = false;
+
+console.log(isChecked && !isFalse);
+
